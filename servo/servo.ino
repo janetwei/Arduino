57b161bb311ceaae71adfd3beb2f74c09a8a4213
janetwei <F61031@gmail.com>
2019-11-20 00:28:08 +0800

    motor example

---
+++ servo/servo.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo myservo; // 建立Servo物件，控制伺服馬達
+
+void setup() 
+{ 
+  myservo.attach(9); // 連接數位腳位9，伺服馬達的訊號線
+} 
+
+void loop() 
+{
+  myservo.write(180);
+  delay(500);
+  myservo.write(0);
+  delay(500);
+}
